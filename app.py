850bd77b725a838db03631ae0d79ef29106a08c0
rodrigo.cabral <rodrigo.cabral@radixeng.com>
2024-09-19 20:19:58 +0000

    primeiro commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import json
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/')
+def index():
+    with open("static/data/personal_data.json", encoding='utf-8') as my_json:
+        personal_data = json.load(my_json)
+        
+    return render_template('index.html',data=personal_data)
