50265237836381d18dbe2926c5bb25b4d26dc48b
rodrigo.cabral <rodrigo.oliveiracabral@hotmail.com>
2024-12-26 02:40:27 +0000

    Add language parameter to routes for multilingual support

--- app.py
+++ app.py
@@ -7,48 +7,52 @@ app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/')
-def index():
+@app.route('/<lang>/')
+def index(lang='pt'):
     with open("static/data/personal_data.json", encoding='utf-8') as my_json:
         personal_data = json.load(my_json)
-        
-    return render_template('index.html',personal_data=personal_data)
+    return render_template('index.html', personal_data=personal_data, lang=lang)
 
 @app.route('/education')
-def education():
+@app.route('/<lang>/education')
+def education(lang='pt'):
     with open("static/data/profissional_skills_data.json", encoding='utf-8') as my_json:
         education = json.load(my_json)
     with open("static/data/personal_data.json", encoding='utf-8') as my_json:
         personal_data = json.load(my_json)
     heading = "Formação"
         
-    return render_template('education.html',data=education, personal_data=personal_data,heading=heading)
+    return render_template('education.html',data=education, personal_data=personal_data,heading=heading, lang=lang)
 
 @app.route('/work')
+@app.route('/<lang>/work')
 def work():
     with open("static/data/profissional_skills_data.json", encoding='utf-8') as my_json:
         work = json.load(my_json)
     with open("static/data/personal_data.json", encoding='utf-8') as my_json:
         personal_data = json.load(my_json)
     heading = "Experiência"
-    return render_template('experience.html',data=work,personal_data=personal_data,heading=heading)
+    return render_template('experience.html',data=work,personal_data=personal_data,heading=heading, lang=lang)
 
 @app.route('/articles')
+@app.route('/<lang>/articles')
 def projects():
     with open("static/data/articles.json", encoding='utf-8') as my_json:
         articles = json.load(my_json)
     with open("static/data/personal_data.json", encoding='utf-8') as my_json:
         personal_data = json.load(my_json)
         
-    return render_template('articles.html',data=articles,personal_data=personal_data)
+    return render_template('articles.html',data=articles,personal_data=personal_data, lang=lang)
 
 @app.route('/repositories')
+@app.route('/<lang>/repositories')
 def repositories():
     with open("static/data/repositories.json", encoding='utf-8') as my_json:
         repositories = json.load(my_json)
     with open("static/data/personal_data.json", encoding='utf-8') as my_json:
         personal_data = json.load(my_json)
         
-    return render_template('repository.html',data=repositories,personal_data=personal_data)
+    return render_template('repository.html',data=repositories,personal_data=personal_data,  lang=lang)
 
 if __name__ == '__main__':
     app.run(port=10000, debug=True)
